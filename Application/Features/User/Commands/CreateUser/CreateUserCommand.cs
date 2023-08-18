d884759b3d848ab11955ca240bef132ca485596d
abneka <anekahiwot@gmail.com>
2023-08-18 22:00:24 +0300

    CQRS features on user entity

---
+++ Application/Features/User/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.User;
+using MediatR;
+
+namespace Application.Features.User.Commands.CreateUser;
+
+public class CreateUserCommand : IRequest<int>
+{
+    public UserRequestDto UserDto { get; set; }
+}
---
+++ Application/Features/User/Queries/GetSingleUser/GetSingleUserRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Contracts;
+using Application.DTOs.User;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.User.Queries.GetSingleUser;
+
+public class GetSingleUserRequestHandler : IRequestHandler<GetSingleUserRequest, UserResponseDto>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public GetSingleUserRequestHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<UserResponseDto> Handle(GetSingleUserRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.Id);
+        return _mapper.Map<UserResponseDto>(user);
+    }
+}
