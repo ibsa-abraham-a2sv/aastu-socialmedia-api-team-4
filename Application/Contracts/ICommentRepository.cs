b9315340ddc8c0ef854532f5e14ea8904c40d30b
haymanotdemis <haymanot.demis@a2sv.org>
2023-08-18 13:20:59 +0300

    Project Reference

--- Application/Contracts/ICommentRepository.cs
+++ Application/Contracts/ICommentRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Contracts.Comment
+namespace Application.Contracts
 {
     public interface ICommentRepository : IGenericRepository<CommentEntity>
     {
--- Application/Features/Comment/Commands/DeleteComment/DeleteCommentCommandHandler.cs
+++ Application/Features/Comment/Commands/DeleteComment/DeleteCommentCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Comment.Commands.DeleteComment
 {
-    internal class DeleteCommentCommandHandler
+    public class DeleteCommentCommandHandler : IRequestHandler<DeleteCommentCommand, Unit>
     {
+        public Task<Unit> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
