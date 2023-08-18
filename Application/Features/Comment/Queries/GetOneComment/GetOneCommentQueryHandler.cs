9a5c9f78ab401e0c9d0e1b27d34d5baa40bc4ebf
haymanotdemis <haymanot.demis@a2sv.org>
2023-08-18 12:57:16 +0300

    Comment Features of CQRS pattern and DI

---
+++ Application/Features/Comment/Queries/GetOneComment/GetOneCommentQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Comment;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Comment.Queries.GetOneComment
+{
+    public class GetOneCommentQueryHandler : IRequestHandler<GetOneCommentQuery, CommentResponseDTO>
+    {
+        public Task<CommentResponseDTO> Handle(GetOneCommentQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
