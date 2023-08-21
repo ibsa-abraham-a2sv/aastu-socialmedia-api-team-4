c82356aa4c73831e6a7ffa7e5f7b7bf700ce62f0
haymanotdemis <haymanot.demis@a2sv.org>
2023-08-21 11:17:13 +0300

    Persistence Layer AppDBContext

--- Persistence/AppDBContext.cs
+++ Persistence/AppDBContext.cs
@@ -20,6 +20,7 @@ namespace Persistence
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<CommentEntity>(entity =>
             {
+                entity.HasKey(e => e.Id);
             });
         }
     }
