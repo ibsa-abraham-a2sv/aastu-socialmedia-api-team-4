a398ba9c555ae406dfa51a04073672d4fc16d769
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-21 13:53:56 +0300

    like controller

---
+++ WebApi/Controllers/LikeController.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs.Like;
+using Application.Features.Like.Commands.Create_Like;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LikeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LikeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<LikeDto>> CreateLike(LikeDto likeDto)
+    {
+        var command = new CreateLikeCommand
+        {
+            LikeDto = likeDto
+        };
+
+        var like = await _mediator.Send(command);
+
+        return CreatedAtAction(null, like);
+    }
+}
