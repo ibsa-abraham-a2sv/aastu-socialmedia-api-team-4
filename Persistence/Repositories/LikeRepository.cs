c8084682a9c01bb2a6c78d14b1840c3af2492b5e
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-21 11:11:15 +0300

    repository implementations for like, follow and notification

---
+++ Persistence/Repositories/LikeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts;
+using Domain.Entities;
+using MediatR;
+
+namespace Persistence.Repositories;
+
+public class LikeRepository : GenericRepository<LikeEntity>
+{
+    private readonly AppDBContext _dbContext;
+
+    public LikeRepository(AppDBContext dbContext) : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+}
