201888594d3028c2bc766122fef3fbc19fd57123
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-23 10:27:27 +0300

    UserConnection table migration created

--- Persistence/Migrations/20230823072134_UserConnectionTableAdded.Designer.cs
+++ Persistence/Migrations/20230823072134_UserConnectionTableAdded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence;
@@ -11,9 +12,11 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230823072134_UserConnectionTableAdded")]
+    partial class UserConnectionTableAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
