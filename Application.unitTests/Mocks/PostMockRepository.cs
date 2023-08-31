f5c49657302ad0a6e30a1d46a9451184f788ea1b
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-31 10:53:33 +0300

    unit test template folder structure update

--- Application.unitTests/Mocks/PostMockRepository.cs
+++ Application.unitTests/Mocks/PostMockRepository.cs
@@ -1,42 +1,10 @@
-﻿using Application.Contracts;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Moq;
 
 namespace Application.unitTests.Mocks;
 
-public static class MockRepositories
+public static class PostMockRepository
 {
-    public static Mock<ILikeRepository> GetLikeRepository()
-    {
-        var likes = new List<LikeEntity>
-        {
-            new LikeEntity
-            {
-                Id = 1,
-                UserId = 1,
-                PostId = 1
-            },
-            new LikeEntity
-            {
-                Id = 2,
-                UserId = 2,
-                PostId = 1
-            },
-            new LikeEntity
-            {
-                Id = 3,
-                UserId = 3,
-                PostId = 1
-            }
-        };
-        
-        var mockLikeRepository = new Mock<ILikeRepository>();
-
-        mockLikeRepository.Setup(m => m.GetAllAsync()).ReturnsAsync(likes);
-
-        return mockLikeRepository;
-    }
-    
     public static Mock<IPostRepository> GetPostRepository()
     {
         var posts = new List<PostEntity>
