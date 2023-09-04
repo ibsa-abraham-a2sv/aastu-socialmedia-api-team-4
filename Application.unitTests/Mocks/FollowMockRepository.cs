f2f94feec825238a11f8c32d1f7efe9635d341a8
Nahom Habtamu <nhabtamu42@gmail.com>
2023-09-04 09:59:45 +0300

    unit test for like and follow

---
+++ Application.unitTests/Mocks/FollowMockRepository.cs
@@ -0,0 +1,47 @@
+﻿using Application.Contracts;
+using Domain.Entities;
+using Moq;
+
+namespace Application.unitTests.Mocks;
+
+public static class FollowMockRepository
+{
+    public static Mock<IFollowRepository> GetFollowRepository()
+    {
+        var follows = new List<FollowEntity>()
+        {
+            new FollowEntity()
+            {
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow,
+                FollowerId = 3,
+                FollowingId = 2
+            },
+            new FollowEntity()
+            {
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow,
+                FollowerId = 1,
+                FollowingId = 2
+            },
+            new FollowEntity()
+            {
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow,
+                FollowerId = 2,
+                FollowingId = 3
+            }
+            
+        };
+
+        var mockFollowRepository = new Mock<IFollowRepository>();
+
+        mockFollowRepository.Setup(f => f.CreateAsync(It.IsAny<FollowEntity>())).ReturnsAsync((FollowEntity follow) =>
+        {
+            follows.Add(follow);
+            return follow;
+        });
+
+        return mockFollowRepository;
+    }
+}
